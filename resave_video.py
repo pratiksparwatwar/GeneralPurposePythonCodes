a9a155913a1f57f55052f1d0231c58c6b2ee9b2e
pratiksparwatwar <53284079+pratiksparwatwar@users.noreply.github.com>
2020-06-25 09:08:17 +0000

    video_save_format

---
+++ resave_video.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy
+
+video_capture = cv2.VideoCapture("bay_12_1.mp4")
+fps = video_capture.get(cv2.CAP_PROP_FPS)
+w = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter("/home/user/Documents/Project/Object_Tracking/videos/bay12/bay_23_converted.mp4", fourcc, fps, (w, h))
+
+while True:
+    ret, frame = video_capture.read()  # frame shape 640*480*3
+    if ret != True:
+        break
+    out.write(frame)
+
+video_capture.release()
+out.release()
+cv2.destroyAllWindows()
+    
