f5bb30b06895066448c381dafd325486d19f77cd
pratiksparwatwar <53284079+pratiksparwatwar@users.noreply.github.com>
2020-06-25 14:40:28 +0530

    Update resave_video.py

--- resave_video.py
+++ resave_video.py
@@ -2,16 +2,18 @@ import cv2
 import numpy
 
 video_capture = cv2.VideoCapture("input.mp4")
+
+#video parameters
 fps = video_capture.get(cv2.CAP_PROP_FPS)
 w = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 h = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-
+#video_extension
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter("output.mp4", fourcc, fps, (w, h))
 
 while True:
-    ret, frame = video_capture.read()  # frame shape 640*480*3
+    ret, frame = video_capture.read()
     if ret != True:
         break
     out.write(frame)
