0d6c513a750c0a24c798083632abdc8dc9b1b878
pratiksparwatwar <53284079+pratiksparwatwar@users.noreply.github.com>
2020-07-15 13:02:49 +0530

    Update changing_json_file.py

--- changing_json_file.py
+++ changing_json_file.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-image_path = "/home/user/Documents/Project/Bajaj_Engine_Project_UI/On_site_data/videos/images/K1BS6_IP/train/k1ip_lot4_labelled/"
+image_path = "/home/ubuntu/Documents/Vikram/AstaMRCNNLabelledIMG/AstaAllImages/"
 
 img_list = os.listdir(image_path)
 img_list.sort()
@@ -9,12 +9,8 @@ for f in img_list:
     if ".json" in str(f):
         with open(image_path + f, 'r') as f1:
             d = json.load(f1)
-            #print(d.keys())
-            for i,j in enumerate(d):
-                if i < 4:
-                    d['shapes'][i]['label'] = d['shapes'][i]['label'].replace("KIBS6", "K1BS6")
-                    print(d['shapes'][i]['label'])        
-                else:
-                    continue
+            for i,j in enumerate(d['shapes']):
+                d['shapes'][i]['label'] = d['shapes'][i]['label'].replace("old", "new")
+                print(d['shapes'][i]['label'])
         with open(image_path + f, 'w') as f2:
-            json.dump(d, f2)
+           json.dump(d, f2)
