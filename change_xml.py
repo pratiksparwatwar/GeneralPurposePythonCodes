0e9c790db208299ff98e12f2858a918e83db1cd3
pratiksparwatwar <53284079+pratiksparwatwar@users.noreply.github.com>
2020-07-18 18:27:42 +0530

    Create change_xml.py

---
+++ change_xml.py
@@ -0,0 +1,21 @@
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+
+def xml_to_csv(path):
+    for xml_file in glob.glob(path + '/*.xml'):
+        with open(xml_file, encoding='latin-1') as f:
+            tree = ET.parse(xml_file)
+            root = tree.getroot()
+            for member in root.findall('object'):
+                member[0].text = member[0].text.replace("TENSE","Tense")
+                # if "washing cast" in member[0].text:
+                # print(member[0].text)
+        tree.write(xml_file, encoding='latin-1')
+def main():
+    image_path = './xml/'
+    xml_df = xml_to_csv(image_path)
+
+main()
